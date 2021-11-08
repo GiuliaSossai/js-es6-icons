0f7dd13e0d597aeb80aaf837e3a0c266f39c35b9
GiuliaSossai <giulia.sossai@yahoo.it>
2021-11-08 17:29:57 +0100

    inizio milestone 2 rifatto

--- milestone-2/js/main.js
+++ milestone-2/js/main.js
@@ -116,8 +116,52 @@ const icons = [
 /**
  * Ciascuna icona ha una proprietà “color”: utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
  */
-const favicons = document.getElementsByClassName('fas');
-//console.log('elenco favicons' , favicons);
+
+createCards();
+
+function createCards(){
+  //resetto l'ambiete dove devo stampare l'elenco la prima volta
+  document.querySelector('.container').innerHTML = '';
+
+  // cilo for of su array per ottenere i cari elementi
+  for(let icon of icons){
+    createHtml(icon);
+  }
+
+}
+
+
+//funzione per creare struttura html di ogni card
+function createHtml(item){
+  console.log('dentro funzione crea html');
+
+  const container = document.querySelector('.container');
+  console.log('container', container);
+  let prevContent = container.innerHTML;
+
+  //destrutturo oggetto icon per accedere alle sue proprietà
+  const {name, prefix, type, family, color} = item;
+  console.log('nome icona', name);
+  console.log('icona', item);
+
+  //definisco struttura html
+  prevContent +=
+  `
+  <div class="box col-2 flex-column py-3 text-center">
+    <div class="item-image">
+      <i class="${family} ${prefix}${name} fs-1"></i>
+    </div>
+    <div class="item-text text-uppercase">
+       <h5>${name}</h5>
+    </div>  
+  </div>
+  `;
+
+  container.innerHTML = prevContent;
+  
+}
+
+
 
 //faccio ciclo su tutto l'array per intercettare i diversi valori di color
 
